720e9ded8612e187b5b1e4c45687707f509ed141
Max Jaffe <maxbjaffe@gmail.com>
2025-11-26 16:09:21 -0500

    EMERGENCY: Disable PWA completely - breaking auth sessions

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: true, // TEMPORARILY DISABLED - PWA breaking auth sessions
   // Include custom service worker code for update handling
   additionalManifestEntries: [
     { url: '/sw-custom.js', revision: Date.now().toString() }
